465f7985318ce5bc6842fad0ebe0998ad881426e
KONSTANTIN VASILEV <vasilev110295@mail.ru>
2022-05-19 07:09:37 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,18 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean1 == bean2);
+        System.out.println(bean3 == bean4);
+
+    }
+}
---
+++ src/main/java/Cat.java
@@ -0,0 +1,6 @@
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cat {
+
+}
